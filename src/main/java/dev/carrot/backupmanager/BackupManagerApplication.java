b621c3f4252bde27d884d49db8bb34bb1643b323
avengerweb <avengerweb@yandex.ru>
2020-05-23 21:45:27 -0700

    Init spring project

---
+++ src/main/java/dev/carrot/backupmanager/BackupManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.carrot.backupmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/carrot/backupmanager/BackupManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carrot.backupmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
